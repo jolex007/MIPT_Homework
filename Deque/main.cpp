522dcf5a1ed03b2c206499bb6db0e5a6c522a93a
jolex007 <jokers3000@gmail.com>
2019-10-04 19:00:04 +0300

    Initial Commit

---
+++ Deque/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Deque
+//
+//  Created by Алексей Зерцалов on 04/10/2019.
+//  Copyright © 2019 Алексей Зерцалов. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
