9ceecc3caba4035c28d6a5ece088d34148095f8d
jolex007 <jokers3000@gmail.com>
2019-10-10 00:55:51 +0300

    BigInteger change

--- BigInteger/BigInteger/biginteger.h
+++ BigInteger/BigInteger/biginteger.h
@@ -11,6 +11,13 @@
 #ifndef biginteger_h
 #define biginteger_h
 
+/*
+ Идея -
+ Как складывать два пофигу каких числа
+ Если оно отрицательное или вычитаем, то переводим в обратное и складываем
+ 
+ */
+
 class BigInteger
 {
 private:
@@ -18,9 +25,58 @@ private:
     unsigned char * data;
     size_t capacity;
     size_t size;
+    char sign;
+    // 1 if positive
+    // -1 if negative
+    
+    static const size_t BEGIN_CAPACITY = 4;
+    
+    // Sum of two BigInteger elements by module
+    BigInteger Sum (const BigInteger & first_element, const BigInteger & second_element)
+    {
+        BigInteger result(std::max(first_element.capacity, second_element.capacity) + 1);
+        
+        size_t size = std::max(first_element.size, second_element.size) + 1;
+        
+        unsigned char carry = 0;
+        
+        for (size_t i = 0; i < size; i++) {
+            result.data[i] = first_element.data[i] + second_element.data[i] + carry;
+            if ((result.data[i] == first_element.data[i] && carry) ||
+                (result.data[i] < first_element.data[i])) {
+                
+                carry = 1;
+            } else {
+                carry = 0;
+            }
+            if (result.data[i]) {
+                result.size = i + 1;
+            }
+        }
+        
+        return result;
+    }
+    
     
 public:
+
+    BigInteger ()
+    {
+        capacity = BEGIN_CAPACITY;
+        data = new unsigned char[capacity];
+        memset(data, 0, capacity * sizeof(unsigned char));
+        size = 0;
+        sign = 1;
+    }
     
+    BigInteger (size_t start_capacity)
+    {
+        capacity = start_capacity;
+        data = new unsigned char[capacity];
+        memset(data, 0, capacity * sizeof(unsigned char));
+        size = 0;
+        sign = 1;
+    }
     
     
     
