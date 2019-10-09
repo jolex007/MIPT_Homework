da325b9156ed3f8c09b4f44194f10ac3be35bf05
jolex007 <jokers3000@gmail.com>
2019-10-09 20:44:33 +0300

    BigInteger added

---
+++ BigInteger/BigInteger/biginteger.h
@@ -0,0 +1,29 @@
+//
+//  biginteger.h
+//  BigInteger
+//
+//  Created by Алексей Зерцалов on 09/10/2019.
+//  Copyright © 2019 Алексей Зерцалов. All rights reserved.
+//
+
+#include <iostream>
+
+#ifndef biginteger_h
+#define biginteger_h
+
+class BigInteger
+{
+private:
+    
+    unsigned char * data;
+    size_t capacity;
+    size_t size;
+    
+public:
+    
+    
+    
+    
+};
+
+#endif /* biginteger_h */
---
+++ BigInteger/BigInteger/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "biginteger.h"
+
+
+
+
+int main()
+{
+    
+    
+    return 0;
+}
