6346f5254e19b17bfad14b7fa97a4a7f377145d4
jolex007 <jokers3000@gmail.com>
2020-06-17 16:16:12 +0300

    Added is_constructible

---
+++ Exams/is_constructible.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+template <typename T, typename U>
+struct is_same {
+    static const bool value = false;
+};
+
+template <typename T>
+struct is_same<T, T> {
+    static const bool value = true;
+};
+
+
+template <typename Class, typename... Args>
+struct is_constructible {
+private:
+    template <typename T, typename... Args2>
+    static decltype(T(Args2()...), char()) f(int x) {
+        return 0;
+    }
+    
+    template <typename...>
+    static int f(...) {
+        return 0;
+    }
+    
+public:
+    static const bool value = is_same<decltype(f<Class, Args...>(0)), char>::value;
+};
+
+struct S {
+    S(int, int);
+};
+
+int main()
+{
+    std::cout << is_constructible<S, int, int>::value;
+    
+    return 0;
+}
